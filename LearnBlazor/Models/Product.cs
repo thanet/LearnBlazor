182460ddf431da45acc815360ca86e5888746251
Thanet_y <thanet_y@yahoo.com>
2025-02-26 10:51:59 +0700

    init

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
